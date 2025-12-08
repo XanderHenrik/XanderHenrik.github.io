68e676a072a567978c74e517b9729c6c7a2628ef
Xander Noordeloos <xandernoordeloos@Xanders-MacBook-Air.local>
2025-12-09 00:51:45 +0100

    Implemented a light and dark mode toggle; made it work now and happy with the position and styling of the button

--- app.js
+++ app.js
@@ -1,3 +1,31 @@
-function alertButton() {
-    alert("whaddup")
+// function alertButton() {
+//     alert("whaddup")
+// }
+
+// Dark Mode Toggle ============================================================
+
+let darkmode = localStorage.getItem("darkmode");
+const themeToggle = document.getElementById("theme-toggle");
+
+const enableDarkMode = () => {
+    document.body.classList.add("darkmode");
+    localStorage.setItem("darkmode", "active");
 }
+
+const disableDarkMode = () => {
+    document.body.classList.remove("darkmode");
+    localStorage.setItem("darkmode", null);
+}
+
+if(darkmode === "active") enableDarkMode();
+
+themeToggle.addEventListener("click", () => {
+    darkmode = localStorage.getItem("darkmode")
+    darkmode !== "active" ? enableDarkMode() : disableDarkMode();
+})
+
+// if(darkmode === null) disableDarkMode();
+//     themeToggle.addEventListener("click", () => {
+//         localStorage.getItem("darkmode")
+//         darkmode === "active" ? disableDarkMode() : enableDarkMode(); 
+// })
